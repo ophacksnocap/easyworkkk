54ac2b9fa80acf1099c2ad7a94814012b4fd9ad8
ophacksnocap <107158338+ophacksnocap@users.noreply.github.com>
2024-10-30 16:11:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Telegram Bot Script for Ticket Transfer
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 import requests
 
 # Function to log into Ticketmaster and transfer tickets
@@ -39,16 +39,9 @@ def handle_message(update, context):
         update.message.reply_text('Error: ' + str(e))
 
 # Main function to run the bot
-def main():
-    # Replace 'YOUR_TOKEN' with your actual Telegram bot token
-    updater = Updater('7297098002:AAFEsskPhNfsyAVvzCUQ8r2hCV9OofRbPEE', use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-
-    updater.start_polling()
-    updater.idle()
+application = Application.builder().token('TOKEN').build()
+application.add_handler(CommandHandler('start', start_callback))
+application.run_polling()
 
 if __name__ == '__main__':
     main()
